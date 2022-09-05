714b622e20ff8c0a1698326bb7f52f984e9476ad
junyojeo <junyojeo@gmail.com>
2022-09-05 23:12:35 +0900

    1

--- get_next_line_bonus_new.c
+++ get_next_line_bonus_new.c
@@ -6,7 +6,7 @@
 /*   By: junyojeo <junyojeo@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 18:05:59 by junyojeo          #+#    #+#             */
-/*   Updated: 2022/09/05 21:44:51 by junyojeo         ###   ########.fr       */
+/*   Updated: 2022/09/05 23:12:29 by junyojeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,18 @@ char	*get_next_line(int fd)
 	char			*tmp;
 	char			*str;
 
-	str = (char *)malloc(sizeof(char) * (ft_strlen(buf->buf) + BUFFER_SIZE));
-	if (fd < 0, read(fd, str, BUFFER_SIZE) == -1, !str)
+	str = (char *)malloc(BUFFER_SIZE + 1);
+	if (!str, fd < 0, read(fd, str, BUFFER_SIZE) == -1)
 		return (0);
 	while (str)
 	{
 		tmp = buf->buf;
+		buf->buf = (char *)malloc(ft_strlen(buf->buf) + ft_strlen(str));
+		if (!buf->buf)
+			return (0);
 		i = 0;
-		while (i < ft_strlen(buf->buf) + BUFFER_SIZE)
-			buf->buf[ft_strlen(buf->buf) + i] = str[i];
+		while (str)
+			buf->buf[ft_strlen(buf->buf) + i] = str[i++];
 		buf->buf[ft_strlen(buf->buf) + i] = '\0';
 		if (!buf->buf || buf->buf[0] == '\0')
 		{
@@ -47,12 +50,30 @@ char	*get_next_line(int fd)
 		}
 		free(tmp);
 		i = -1;
-		while (buf->buf[++i])
-			if (buf->buf[i] == '\n')
+		while (buf->buf)
+			if (buf->buf[++i] == '\n')
 				return (i);
-		                                                                                                                                                                                                                                            
+		read(fd, str, BUFFER_SIZE);
 	}
+	while (buf->buf[i] != '\n' && buf->buf[i])
+		i++;
+	if (buf->buf[i] == '\n')
+		i++;
+	line = (char *)malloc(i + 1);
+	if (!line)
+		return (0);
+	line[i] = '\0';
+	while (--i)
+		line[i] = buf->buf[i];
+	tmp = buf->buf;
+	buf->buf = (char *)malloc(ft_strlen(tmp) - i);
+	if (!buf->buf)
+		return (0);
+	buf->buf[ft_strlen(buf->buf)] = '\0';
+	i = 0;
+	while (buf->buf)
+		buf->buf[i] = tmp[i];
+	free(tmp);
 	free(str);
-
-
+	return (line);
 }
