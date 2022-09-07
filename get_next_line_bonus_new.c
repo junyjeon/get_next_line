3c6a70407be2aac1ed8013677f9f32f9de3dae43
junyjeon <98337069+junyjeon@users.noreply.github.com>
2022-09-08 00:10:29 +0900

    Update get_next_line_bonus_new.c

--- get_next_line_bonus_new.c
+++ get_next_line_bonus_new.c
@@ -18,19 +18,19 @@ typedef struct s_node
 	struct t_node	*next;
 	ssize_t			read_size;
 	int				fd;
-	char			buf[BUFFER_SIZE];
+	char			*buf;
 }	t_node;
 
 char	*get_next_line(int fd)
 {
-	static t_node	*buf = {.head = buf .fd = fd, .next = NULL};
+	static t_node	*buf = {.head = buf, .fd = fd, .next = NULL};
 	t_node			*tmp_node;
 	int				i;
 	char			*tmp;
 	char			*str;
 
 	str = (char *)malloc(BUFFER_SIZE + 1);
-	if (!str, fd < 0, read(fd, str, BUFFER_SIZE) == -1)
+	if (!str || fd < 0 || read(fd, str, BUFFER_SIZE) == -1)
 		return (0);
 	while (str)
 	{
@@ -39,7 +39,7 @@ char	*get_next_line(int fd)
 		if (!buf->buf)
 			return (0);
 		i = 0;
-		while (str)
+		while (str[i])
 			buf->buf[ft_strlen(buf->buf) + i] = str[i++];
 		buf->buf[ft_strlen(buf->buf) + i] = '\0';
 		if (!buf->buf || buf->buf[0] == '\0')
@@ -50,30 +50,27 @@ char	*get_next_line(int fd)
 		}
 		free(tmp);
 		i = -1;
-		while (buf->buf)
+		while (buf->buf[i])
 			if (buf->buf[++i] == '\n')
 				return (i);
-		read(fd, str, BUFFER_SIZE);
+		if (!read(fd, str, BUFFER_SIZE))
+                        return (0);
 	}
-	while (buf->buf[i] != '\n' && buf->buf[i])
-		i++;
-	if (buf->buf[i] == '\n')
-		i++;
 	line = (char *)malloc(i + 1);
 	if (!line)
 		return (0);
 	line[i] = '\0';
-	while (--i)
-		line[i] = buf->buf[i];
-	tmp = buf->buf;
-	buf->buf = (char *)malloc(ft_strlen(tmp) - i);
-	if (!buf->buf)
+        j = 0;
+	while (j < i)
+		line[j] = buf->buf[j++];
+	tmp = str;
+	str = (char *)malloc(ft_strlen(buf->buf) - i);
+	if (!str)
 		return (0);
-	buf->buf[ft_strlen(buf->buf)] = '\0';
-	i = 0;
-	while (buf->buf)
-		buf->buf[i] = tmp[i];
+	str[ft_strlen(line)] = '\0';
+	while (buf->buf[i])
+		buf->buf[i] = tmp[i + j];
 	free(tmp);
-	free(str);
+	lst_clear();
 	return (line);
 }
